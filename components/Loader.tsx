ea7b07fa1f3ed9adad3f7c3910e9b17d09de5de2
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2023-09-06 13:00:41 +0530

    added loading and error screen

---
+++ components/Loader.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { PuffLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div
+      className="
+        h-[70vh]
+        flex 
+        flex-col 
+        justify-center 
+        items-center 
+      "
+    >
+      <PuffLoader size={100} color="#FC595C" />
+    </div>
+  );
+};
+
+export default Loader;
