5019184cfbe13153f4d7889d4b2e56211d6a9cff
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2023-09-08 23:21:12 +0530

    fixed ts error issues

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -16,7 +16,7 @@ export type SafeReservation = Omit<
 
 export type SafeUser = Omit<
   User,
-  "created_at" | "updated_at" | "email_verified"
+  "created_at" | "updated_at" | "emailVerified"
 > & {
   created_at: string;
   updated_at: string;
